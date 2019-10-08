0ee1334b775cda1a99e21606f3259dd74d61a164
WXY <547535278@qq.com>
2019-10-08 12:00:39 +0800

    update

---
+++ src/api/message.js
@@ -0,0 +1,41 @@
+import request from '@/utils/request'
+
+/**
+ *  获取短信列表
+ */
+
+export function getMessageList(query) {
+    var url = '/sms?' + 'pageIndex=' + query.pageIndex + '&pageSize=' + query.pageSize
+    if (query.id) { // 用户id
+        url += '&id=' + query.id
+    }
+    if (query.status) { // 等级
+        url += '&status=' + query.status
+    }
+    if (query.userId) { // 邀请码（用户id）
+        url += '&userId=' + query.userId
+    }
+    if (query.userPhone) { // 手机号
+        url += '&userPhone=' + query.userPhone
+    }
+    return request({
+        url: url,
+        method: 'get'
+    })
+}
+
+// 通过id获取短信详情
+export function getMessageById(id) {
+    return request({
+        url: '/sms/' + id,
+        method: 'get'
+    })
+}
+
+// 删除短信
+export function delMessage(unionid) {
+    return request({
+        url: '/sms/' + unionid,
+        method: 'delete'
+    })
+}
--- src/views/message/list.vue
+++ src/views/message/list.vue
@@ -8,34 +8,22 @@
         <el-input v-model="formInline.id"
                   placeholder="id"></el-input>
       </el-form-item>
-      <el-form-item label="电话号码">
-        <el-input v-model="formInline.phone"
-                  placeholder="电话号码"></el-input>
+      <el-form-item label="电话 ">
+        <el-input v-model="formInline.userPhone"
+                  placeholder="电话 "></el-input>
       </el-form-item>
-      <el-form-item label="等级">
-        <el-input v-model="formInline.rank"
-                  placeholder="等级"></el-input>
+      <el-form-item label="用户id">
+        <el-input v-model="formInline.userId"
+                  placeholder="用户id"></el-input>
       </el-form-item>
-      <el-form-item label="用户状态 ">
+      <el-form-item label="发送状态  ">
         <el-select v-model="formInline.status"
-                   placeholder="用户状态"
+                   placeholder="发送状态 "
                    @change="onSubmit">
-          <el-option label="正常使用过 "
+          <el-option label="失败  "
+                     value="0"></el-option>
+          <el-option label="成功"
                      value="1"></el-option>
-          <el-option label="暂时停用"
-                     value="2"></el-option>
-          <el-option label="永久停用"
-                     value="3"></el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="用户类型 ">
-        <el-select v-model="formInline.type"
-                   placeholder="用户类型 "
-                   @change="onSubmit">
-          <el-option label="普通用户 "
-                     value="1"></el-option>
-          <el-option label="平台用户"
-                     value="2"></el-option>
         </el-select>
       </el-form-item>
       <el-form-item>
@@ -45,66 +33,45 @@
     </el-form>
 
     <el-table :data="tableData">
-      <!-- 用户信息 -->
+      <!-- 短信信息 -->
       <el-table-column prop="id"
                        label="id"
                        width="250"></el-table-column>
-      <el-table-column prop="name"
-                       label="昵称"
+      <el-table-column prop="userId"
+                       label="用户id"
                        width="200"></el-table-column>
-      <el-table-column prop="phone"
+      <el-table-column prop="userName"
+                       label="用户名"
+                       width="200"></el-table-column>
+      <el-table-column prop="userPhone"
                        label="电话"
+                       width="150"></el-table-column>
+      <el-table-column prop="status"
+                       label="发送状态"
                        width="150"></el-table-column>
       <el-table-column label="头像"
                        width="150">
         <template slot-scope="scope"><img v-image-preview
                style="width: 35px; height: 35px"
-               :src="scope.row.img"
+               :src="scope.row.userImg"
                fit="fill" /></template>
       </el-table-column>
-      <el-table-column prop="rank"
-                       label="等级"
+      <el-table-column prop="param"
+                       label="短信内容"
                        width="150"></el-table-column>
-      <el-table-column prop="status"
-                       label="用户状态"
-                       width="150"></el-table-column>
-      <el-table-column prop="type"
-                       label="用户类型"
-                       width="150"></el-table-column>
-      <el-table-column prop="wechatCode"
-                       label="微信号码"
-                       width="150"></el-table-column>
-      <el-table-column label="创建时间"
+      <el-table-column label="发送时间"
                        width="150">
         <template slot-scope="scope">{{ parseTime(scope.row.time) }}</template>
       </el-table-column>
-
-      <!-- 推荐人信息 -->
-      <el-table-column prop="masterId"
-                       label="推荐人id"
-                       width="150"></el-table-column>
-      <el-table-column prop="masterName"
-                       label="推荐人姓名"
-                       width="150"></el-table-column>
-      <el-table-column label="头像"
-                       width="150">
-        <template slot-scope="scope"><img v-image-preview
-               style="width: 35px; height: 35px"
-               :src="scope.row.masterImg"
-               fit="fill" /></template>
-      </el-table-column>
-      <el-table-column prop="masterPhone"
-                       label="推荐人电话"
-                       width="170"></el-table-column>
-      <!-- <el-table-column fixed="right"
+      <el-table-column fixed="right"
                        label="操作"
                        width="120">
         <template slot-scope="scope">
-          <el-button @click.native.prevent="showList(scope.row.openid)"
+          <el-button @click.native.prevent="showList(scope.row.id)"
                      type="text"
-                     size="small">更改当前用户状态</el-button>
+                     size="small">查看详情</el-button>
         </template>
-      </el-table-column> -->
+      </el-table-column>
     </el-table>
     <!-- 分页区 -->
 
@@ -121,27 +88,33 @@
     </div>
 
     <!--  查看区域  -->
-    <!-- <el-dialog title="用户购买课程列表"
+    <el-dialog title="短信内容详情"
                :visible.sync="showView"
                width="80%">
       <el-table :data="payclass">
-        <el-table-column prop="id"
+        <el-table-column prop="userId"
                          label="课程id"
                          width="300"></el-table-column>
-        <el-table-column prop="name"
+        <el-table-column prop="userPhone"
+                         label="课程名称"
+                         width="300"></el-table-column>
+        <el-table-column prop="userPhone"
+                         label="课程名称"
+                         width="300"></el-table-column>
+        <el-table-column prop="userPhone"
                          label="课程名称"
                          width="300"></el-table-column>
         <el-table-column prop="price"
                          label="课程价格"
                          width="300"></el-table-column>
       </el-table>
-    </el-dialog> -->
+    </el-dialog>
 
   </div>
 </template>
 
 <script>
-import { getUserList, getClassById, delUser } from "@/api/user";
+import { getMessageList, getMessageById, delMessage } from "@/api/message";
 import { parseTime } from "@/utils/index"
 
 export default {
@@ -158,15 +131,14 @@ export default {
       // 搜索内容
       formInline: {
         status: null,
-        type: null,
-        rank: null,
-        id: null,
-        phone: null
+        userId: null,
+        userPhone: null,
+        id: null
       },
     }
   },
   mounted () {
-    this.getUserList()
+    this.getMessageList()
   },
 
   created () {
@@ -177,31 +149,30 @@ export default {
     handleSizeChange (e) {
       // console.log('当前每页数量', e)
       this.pageSize = e
-      this.getUserList()
+      this.getMessageList()
     },
     // 分页改变 e点击的页码  用户手动输入了页面然后go
     handleCurrentChange (e) {
       // console.log('当前页码', e)
       this.pageindex = e - 1
-      this.getUserList()
+      this.getMessageList()
     },
     // 搜索
     onSubmit () {
-      this.getUserList()
+      this.getMessageList()
     },
     // 获取用户列表
-    getUserList () {
+    getMessageList () {
       let query = {
         pageIndex: this.pageindex,
         pageSize: this.pageSize,
-        rank: this.formInline.rank,
-        id: this.formInline.id,
-        phone: this.formInline.phone,
         status: this.formInline.status,
-        type: this.formInline.type
+        id: this.formInline.id,
+        userId: this.formInline.userId,
+        userPhone: this.formInline.userPhone
       }
-      getUserList(query).then(res => {
-        console.log('获取到的用户列表', res)
+      getMessageList(query).then(res => {
+        console.log('获取到的短信列表', res)
         this.tableData = res.data
         this.total = res.pageTotal
       })
@@ -209,12 +180,7 @@ export default {
     // 格式化时间
     parseTime (time) {
       return parseTime(time)
-    },
-    toPut (id) {
-      console.log(id)
-      this.$router.push({ path: 'userClass/put/' + id })
     }
-
   }
 }
 </script>
